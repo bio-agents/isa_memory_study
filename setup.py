47860e8ffb5a750d2c003aa965e76a96984c105f
johnson.chen <joy6677@qq.com>
2024-09-18 16:02:08 +0800

    Update content and rename

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setupagents import find_packages, setup
+
+setup(
+    name='isamemorystudy',
+    packages=find_packages(),
+    version='0.2.0',
+    description='A small study on the memory footprint size used by ISA-API for experimental DAGs.',
+    author='David Johnson',
+    license='MIT',
+)
